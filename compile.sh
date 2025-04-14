fda00593f90f16723e5f586f4ef8e422c98ff856
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-04-14 22:35:54 +0200

    feat: compile step range

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 stepInstall() {
-    oldPath=`pwd`
+    oldPath=$(pwd)
     echo "$(tput setaf 6)Installing ESP-IDF...$(tput sgr0)"
     git clone --recursive https://github.com/espressif/esp-idf.git ~/esp-idf
     resCompile=$?
@@ -16,7 +16,7 @@ stepInstall() {
 }
 
 step0() {
-    oldPath=`pwd`
+    oldPath=$(pwd)
     echo "$(tput setaf 6)Step 0: Preparing...$(tput sgr0)"
     cd ~/esp-idf
     . export.sh
@@ -32,62 +32,88 @@ step0() {
 }
 
 step1() {
-    echo "$(tput setaf 6)Step 1: Compiling...$(tput sgr0)"
-    #
+    echo "$(tput setaf 6)Step 1: Configuring...$(tput sgr0)"
+    . $HOME/esp-idf/export.sh
+    idf.py set-target esp32s3
+    # idf.py menuconfig
     resCompile=$?
     if [[ $resCompile != 0 ]]; then
         echo -ne "$(tput setaf 9)$(tput bold)FAILED at STEP 1:\n$(tput sgr0)$(tput setaf 9)"
+        echo "Error while configuring !"
+        echo -ne "$(tput sgr0)"
+        exit $resCompile
+    fi
+    echo "$(tput setaf 10)Step 1: Successfully configured!$(tput sgr0)"
+}
+
+step2() {
+    echo "$(tput setaf 6)Step 2: Compiling...$(tput sgr0)"
+    #
+    resCompile=$?
+    if [[ $resCompile != 0 ]]; then
+        echo -ne "$(tput setaf 9)$(tput bold)FAILED at STEP 2:\n$(tput sgr0)$(tput setaf 9)"
         echo "Error while compiling !"
         echo -ne "$(tput sgr0)"
         exit $resCompile
     fi
-    echo "$(tput setaf 10)Step 1: Successfully compiled!$(tput sgr0)"
+    echo "$(tput setaf 10)Step 2: Successfully compiled!$(tput sgr0)"
 }
 
 stepi() {
     case $1 in
-        0)
-            step0;;
+    0)
+        step0
+        ;;
 
-        1)
-            step1;;
-        
-        install)
-            stepInstall;;
-        
-        compile)
-            step1;;
+    1)
+        step1
+        ;;
 
-        *)
-            echo -ne "$(tput setaf 9)$(tput bold)"
-            echo "Invalid step $1"
-            echo -ne "$(tput sgr0)"
-            exit 2;;
+    2)
+        step2
+        ;;
+
+    install)
+        stepInstall
+        ;;
+
+    compile)
+        step1
+        step2
+        ;;
+
+    *)
+        echo -ne "$(tput setaf 9)$(tput bold)"
+        echo "Invalid step $1"
+        echo -ne "$(tput sgr0)"
+        exit 2
+        ;;
     esac
 }
 
-
 allSteps() {
-    for i in `seq 0 1`
-    do
+    for i in $(seq 0 1); do
         stepi $i
     done
 }
 
 ###########################################################################
 
-if [[ $# != 0 && $# != 1 ]]; then
+if [[ $# > 2 ]]; then
     echo -ne "$(tput setaf 9)$(tput bold)"
     echo "You need to indicate which project to compile !"
     echo -ne "$(tput sgr0)"
     exit 2
 fi
 
-
-if [[ $# != 1 ]]; then
+if [[ $# = 0 ]]; then
     allSteps
-else
+elif [[ $# = 1 ]]; then
     stepi $1
+else
+    for i in $(seq $1 $2); do
+        stepi $i
+    done
 fi
 
 echo "> $(tput setaf 10)$(tput bold)FINISHED!$(tput sgr0)"
