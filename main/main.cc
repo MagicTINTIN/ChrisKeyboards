c15e2eb8d7d8a98825d6e2c0dd3e0b4c9d3efdd9
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-04-19 23:09:52 +0200

    MAIS PUTAIN !!!

--- main/main.cc
+++ main/main.cc
@@ -31,6 +31,10 @@ const uint8_t hid_report_descriptor[] = {
     // TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(HID_ITF_PROTOCOL_MOUSE)),
     TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(HID_ITF_PROTOCOL_KEYBOARD))};
 
+const uint8_t hid_consumer_report_descriptor[] = {
+    TUD_HID_REPORT_DESC_CONSUMER(HID_REPORT_ID(2)),
+};
+
 /**
  * @brief String descriptor
  */
@@ -49,10 +53,12 @@ const char *hid_string_descriptor[5] = {
  */
 static const uint8_t hid_configuration_descriptor[] = {
     // Configuration number, interface count, string index, total length, attribute, power in mA
-    TUD_CONFIG_DESCRIPTOR(1, 1, 0, TUSB_DESC_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+    TUD_CONFIG_DESCRIPTOR(1, 2, 0, TUSB_DESC_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
 
     // Interface number, string index, boot protocol, report descriptor len, EP In address, size & polling interval
-    TUD_HID_DESCRIPTOR(0, 4, false, sizeof(hid_report_descriptor), 0x81, 16, 10),
+    // FIXME: booot protocol ?
+    // TUD_HID_DESCRIPTOR(0, 4, false, sizeof(hid_report_descriptor), 0x81, 16, 10),
+    TUD_HID_DESCRIPTOR(0, 0, false, sizeof(hid_consumer_report_descriptor), 0x81, CFG_TUD_HID_EP_BUFSIZE, 5),
 };
 
 /********* TinyUSB HID callbacks ***************/
@@ -227,7 +233,14 @@ void languageKeysRegistration(uint8_t k)
 
 void hidUsageKeysRegistration(uint8_t k)
 {
-    
+    // uint16_t data = HID_USAGE_CONSUMER_SCAN_NEXT;
+    uint16_t data = HID_USAGE_CONSUMER_PLAY_PAUSE;
+    tud_hid_report(2, &data, 2);
+    printf("playpaused ?\n");
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+    data = 0;
+
+    tud_hid_report(2, &data, 2);
 }
 
 void otherHidKeysRegistration(uint8_t k)
@@ -257,6 +270,7 @@ void otherHidKeysRegistration(uint8_t k)
 
 void fnKeyPressRegistration(uint8_t k)
 {
+    printf("fnKeyReg %x\n", k);
     // Already pressed keys are priorised
     if (k == 0)
         return;
