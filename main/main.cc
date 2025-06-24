cb48ace13463c470c2ed9a5a596b9a8cddda0b69
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-06-24 21:10:34 +0200

    build: compiling including the ble lib

--- main/main.cc
+++ main/main.cc
@@ -13,6 +13,23 @@
 // #include "esp_task_wdt.h"
 // #include <idf_additions.h>
 
+#include "nvs_flash.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gap_ble_api.h"
+#include "esp_hidd_prf_api.h"
+#include "hid_dev.h"
+#include <stdio.h>
+#include <string.h>
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_bt_defs.h"
+#include "esp_gatts_api.h"
+#include "esp_gatt_defs.h"
+
 #define BUZZER_GPIO 2
 #define BUZZER_CHANNEL LEDC_CHANNEL_0
 #define BUZZER_TIMER LEDC_TIMER_0
