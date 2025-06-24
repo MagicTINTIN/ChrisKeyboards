16413f84bd9f0c8f6a483f6a1ab58ef2ab4cbccf
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-06-24 23:22:21 +0200

    feat: bluetooth transmission of normal keys

--- main/main.cc
+++ main/main.cc
@@ -149,29 +149,43 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
 // {
 // }
 
-#define HIDD_DEVICE_NAME            "ChrisT1 Clavier"
-#define HID_DEMO_TAG "BT" //TODO: remove
+#define HIDD_DEVICE_NAME "ChrisT1 Clavier"
+#define HID_DEMO_TAG "BT" // TODO: remove
 static uint16_t hid_conn_id = 0;
 static bool sec_conn = false;
-
 
 #define GPIO_CAPS_LED GPIO_NUM_21
 
 static uint8_t hidd_service_uuid128[] = {
     /* LSB <--------------------------------------------------------------------------------> MSB */
-    //first uuid, 16bit, [12],[13] is the value
-    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0x12, 0x18, 0x00, 0x00,
+    // first uuid, 16bit, [12],[13] is the value
+    0xfb,
+    0x34,
+    0x9b,
+    0x5f,
+    0x80,
+    0x00,
+    0x00,
+    0x80,
+    0x00,
+    0x10,
+    0x00,
+    0x00,
+    0x12,
+    0x18,
+    0x00,
+    0x00,
 };
 
 static esp_ble_adv_data_t hidd_adv_data = {
     .set_scan_rsp = false,
     .include_name = true,
     .include_txpower = true,
-    .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
-    .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
-    .appearance = 0x03c0,       //HID Generic,
+    .min_interval = 0x0006, // slave connection min interval, Time = min_interval * 1.25 msec
+    .max_interval = 0x0010, // slave connection max interval, Time = max_interval * 1.25 msec
+    .appearance = 0x03c0,   // HID Generic,
     .manufacturer_len = 0,
-    .p_manufacturer_data =  NULL,
+    .p_manufacturer_data = NULL,
     .service_data_len = 0,
     .p_service_data = NULL,
     .service_uuid_len = sizeof(hidd_service_uuid128),
@@ -179,91 +193,100 @@ static esp_ble_adv_data_t hidd_adv_data = {
     .flag = 0x6,
 };
 
-
 static esp_ble_adv_params_t hidd_adv_params = {
-    .adv_int_min        = 0x20,
-    .adv_int_max        = 0x30,
-    .adv_type           = ADV_TYPE_IND,
-    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    .adv_int_min = 0x20,
+    .adv_int_max = 0x30,
+    .adv_type = ADV_TYPE_IND,
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
     //.peer_addr            =
     //.peer_addr_type       =
-    .channel_map        = ADV_CHNL_ALL,
+    .channel_map = ADV_CHNL_ALL,
     .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
 };
 
-
 static void hidd_event_callback(esp_hidd_cb_event_t event, esp_hidd_cb_param_t *param)
 {
-    switch(event) {
-        case ESP_HIDD_EVENT_REG_FINISH: {
-            if (param->init_finish.state == ESP_HIDD_INIT_OK) {
-                //esp_bd_addr_t rand_addr = {0x04,0x11,0x11,0x11,0x11,0x05};
-                esp_ble_gap_set_device_name(HIDD_DEVICE_NAME);
-                esp_ble_gap_config_adv_data(&hidd_adv_data);
+    switch (event)
+    {
+    case ESP_HIDD_EVENT_REG_FINISH:
+    {
+        if (param->init_finish.state == ESP_HIDD_INIT_OK)
+        {
+            // esp_bd_addr_t rand_addr = {0x04,0x11,0x11,0x11,0x11,0x05};
+            esp_ble_gap_set_device_name(HIDD_DEVICE_NAME);
+            esp_ble_gap_config_adv_data(&hidd_adv_data);
+        }
+        break;
+    }
+    case ESP_BAT_EVENT_REG:
+    {
+        break;
+    }
+    case ESP_HIDD_EVENT_DEINIT_FINISH:
+        break;
+    case ESP_HIDD_EVENT_BLE_CONNECT:
+    {
+        ESP_LOGI(HID_DEMO_TAG, "ESP_HIDD_EVENT_BLE_CONNECT");
+        hid_conn_id = param->connect.conn_id;
+        break;
+    }
+    case ESP_HIDD_EVENT_BLE_DISCONNECT:
+    {
+        sec_conn = false;
+        ESP_LOGI(HID_DEMO_TAG, "ESP_HIDD_EVENT_BLE_DISCONNECT");
+        esp_ble_gap_start_advertising(&hidd_adv_params);
+        break;
+    }
+    case ESP_HIDD_EVENT_BLE_VENDOR_REPORT_WRITE_EVT:
+    {
+        ESP_LOGI(HID_DEMO_TAG, "%s, ESP_HIDD_EVENT_BLE_VENDOR_REPORT_WRITE_EVT", __func__);
+        ESP_LOG_BUFFER_HEX(HID_DEMO_TAG, param->vendor_write.data, param->vendor_write.length);
+        break;
+    }
+    case ESP_HIDD_EVENT_BLE_LED_REPORT_WRITE_EVT:
+    {
+        ESP_LOGI(HID_DEMO_TAG, "ESP_HIDD_EVENT_BLE_LED_REPORT_WRITE_EVT");
+        ESP_LOG_BUFFER_HEX(HID_DEMO_TAG, param->led_write.data, param->led_write.length);
+        if (param->led_write.length < 1)
+            break;
 
-            }
-            break;
-        }
-        case ESP_BAT_EVENT_REG: {
-            break;
-        }
-        case ESP_HIDD_EVENT_DEINIT_FINISH:
-	     break;
-		case ESP_HIDD_EVENT_BLE_CONNECT: {
-            ESP_LOGI(HID_DEMO_TAG, "ESP_HIDD_EVENT_BLE_CONNECT");
-            hid_conn_id = param->connect.conn_id;
-            break;
-        }
-        case ESP_HIDD_EVENT_BLE_DISCONNECT: {
-            sec_conn = false;
-            ESP_LOGI(HID_DEMO_TAG, "ESP_HIDD_EVENT_BLE_DISCONNECT");
-            esp_ble_gap_start_advertising(&hidd_adv_params);
-            break;
-        }
-        case ESP_HIDD_EVENT_BLE_VENDOR_REPORT_WRITE_EVT: {
-            ESP_LOGI(HID_DEMO_TAG, "%s, ESP_HIDD_EVENT_BLE_VENDOR_REPORT_WRITE_EVT", __func__);
-            ESP_LOG_BUFFER_HEX(HID_DEMO_TAG, param->vendor_write.data, param->vendor_write.length);
-            break;
-        }
-        case ESP_HIDD_EVENT_BLE_LED_REPORT_WRITE_EVT: {
-            ESP_LOGI(HID_DEMO_TAG, "ESP_HIDD_EVENT_BLE_LED_REPORT_WRITE_EVT");
-            ESP_LOG_BUFFER_HEX(HID_DEMO_TAG, param->led_write.data, param->led_write.length);
-            if (param->led_write.length < 1 ) break;
-
-            uint8_t leds =  param->led_write.data[0];
-            bool caps_on = leds & KEYBOARD_LED_CAPSLOCK;
-            gpio_set_level(GPIO_CAPS_LED, caps_on);
-            break;
-        }
-        default:
-            break;
+        uint8_t leds = param->led_write.data[0];
+        bool caps_on = leds & KEYBOARD_LED_CAPSLOCK;
+        gpio_set_level(GPIO_CAPS_LED, caps_on);
+        break;
+    }
+    default:
+        break;
     }
     return;
 }
 
 static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
 {
-    switch (event) {
+    switch (event)
+    {
     case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
         esp_ble_gap_start_advertising(&hidd_adv_params);
         break;
-     case ESP_GAP_BLE_SEC_REQ_EVT:
-        for(int i = 0; i < ESP_BD_ADDR_LEN; i++) {
-             ESP_LOGD(HID_DEMO_TAG, "%x:",param->ble_security.ble_req.bd_addr[i]);
+    case ESP_GAP_BLE_SEC_REQ_EVT:
+        for (int i = 0; i < ESP_BD_ADDR_LEN; i++)
+        {
+            ESP_LOGD(HID_DEMO_TAG, "%x:", param->ble_security.ble_req.bd_addr[i]);
         }
         esp_ble_gap_security_rsp(param->ble_security.ble_req.bd_addr, true);
-	 break;
-     case ESP_GAP_BLE_AUTH_CMPL_EVT:
+        break;
+    case ESP_GAP_BLE_AUTH_CMPL_EVT:
         sec_conn = true;
         esp_bd_addr_t bd_addr;
         memcpy(bd_addr, param->ble_security.auth_cmpl.bd_addr, sizeof(esp_bd_addr_t));
-        ESP_LOGI(HID_DEMO_TAG, "remote BD_ADDR: %08x%04x",\
-                (bd_addr[0] << 24) + (bd_addr[1] << 16) + (bd_addr[2] << 8) + bd_addr[3],
-                (bd_addr[4] << 8) + bd_addr[5]);
+        ESP_LOGI(HID_DEMO_TAG, "remote BD_ADDR: %08x%04x",
+                 (bd_addr[0] << 24) + (bd_addr[1] << 16) + (bd_addr[2] << 8) + bd_addr[3],
+                 (bd_addr[4] << 8) + bd_addr[5]);
         ESP_LOGI(HID_DEMO_TAG, "address type = %d", param->ble_security.auth_cmpl.addr_type);
-        ESP_LOGI(HID_DEMO_TAG, "pair status = %s",param->ble_security.auth_cmpl.success ? "success" : "fail");
-        if(!param->ble_security.auth_cmpl.success) {
-            ESP_LOGE(HID_DEMO_TAG, "fail reason = 0x%x",param->ble_security.auth_cmpl.fail_reason);
+        ESP_LOGI(HID_DEMO_TAG, "pair status = %s", param->ble_security.auth_cmpl.success ? "success" : "fail");
+        if (!param->ble_security.auth_cmpl.success)
+        {
+            ESP_LOGE(HID_DEMO_TAG, "fail reason = 0x%x", param->ble_security.auth_cmpl.fail_reason);
         }
         break;
     default:
@@ -271,26 +294,26 @@ static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param
     }
 }
 
-
-
 void tud_hid_set_report_cb(uint8_t instance,
                            uint8_t report_id,
                            hid_report_type_t report_type,
-                           const uint8_t* buffer,
+                           const uint8_t *buffer,
                            uint16_t bufsize)
 {
-    if ( instance != 0 ) return;                 // keyboard is interface 0
-    if ( report_type != HID_REPORT_TYPE_OUTPUT ) return;
-    if ( bufsize < 1 ) return;
+    if (instance != 0)
+        return; // keyboard is interface 0
+    if (report_type != HID_REPORT_TYPE_OUTPUT)
+        return;
+    if (bufsize < 1)
+        return;
 
     uint8_t leds = buffer[0];
 
-    bool caps_on = leds & KEYBOARD_LED_CAPSLOCK;  // from hid.h bitmask
+    bool caps_on = leds & KEYBOARD_LED_CAPSLOCK; // from hid.h bitmask
     // printf("Caps LED %s\n", caps_on ? "ON" : "OFF");
 
     gpio_set_level(GPIO_CAPS_LED, caps_on);
 }
-
 
 void buzzer_init()
 {
@@ -488,7 +511,10 @@ void printKeys()
 void sendKeysReport()
 {
     if (!noKeyPressed || !noKeyPressedPreviously)
+    {
         tud_hid_keyboard_report(0, currentMod, currentKeys);
+        esp_hidd_send_keyboard_value(hid_conn_id, currentMod, currentKeys, NUMBER_OF_SIMULT_KEYS);
+    }
     // tud_hid_keyboard_report(HID_ITF_PROTOCOL_KEYBOARD, currentMod, currentKeys);
 
     if (!noConsumerPressed)
@@ -525,7 +551,7 @@ void normalKeyPressRegistration(uint8_t k)
 {
     noKeyPressed = false;
     bool alreadyPressed = alreadyPressedKeys[k];
-    
+
     // printf("k=%d;%d -> [(%d;%d),(%d;%d),(%d;%d),(%d;%d),(%d;%d)...]\n", k, alreadyPressedKeys[k], currentKeys[0], alreadyPressedKeys[currentKeys[0]], currentKeys[1], alreadyPressedKeys[currentKeys[1]], currentKeys[2], alreadyPressedKeys[currentKeys[2]], currentKeys[3], alreadyPressedKeys[currentKeys[3]], currentKeys[4], alreadyPressedKeys[currentKeys[4]]);
     if (!alreadyPressed)
     {
@@ -688,18 +714,18 @@ void keyPressRegistration(uint8_t c, uint8_t r)
 void keyUpdateRegistration()
 {
     // esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_UP, true); it does work lol
-    printf("up?");
+    // printf("up?");
     fflush(stdout);
     for (uint8_t i = 0; i < NUMBER_OF_SIMULT_KEYS; i++)
     {
         alreadyPressedKeys[currentKeys[i]] = 0;
         // NOTE: improve it to only disable when we really need to disable them
-        uint8_t n = 0;
-        for (; n < NUMBER_OF_SIMULT_KEYS; n++)
-            if (newKeys[n] == currentKeys[i])
-                break;
-        if (n == NUMBER_OF_SIMULT_KEYS)
-            esp_hidd_send_consumer_value(hid_conn_id, currentKeys[i], false);
+        // uint8_t n = 0;
+        // for (; n < NUMBER_OF_SIMULT_KEYS; n++)
+        //     if (newKeys[n] == currentKeys[i])
+        //         break;
+        // if (n == NUMBER_OF_SIMULT_KEYS)
+        //     esp_hidd_send_consumer_value(hid_conn_id, currentKeys[i], false);
     }
     uint8_t *tmp = currentKeys;
     currentKeys = newKeys;
@@ -724,7 +750,8 @@ void keyUpdateRegistration()
     alreadyPressedNewKeysFull = false;
     for (uint8_t i = 0; i < NUMBER_OF_SIMULT_KEYS; i++)
     {
-        if (currentKeys[i] > 0) {
+        if (currentKeys[i] > 0)
+        {
             alreadyPressedKeys[currentKeys[i]] = 1;
             esp_hidd_send_consumer_value(hid_conn_id, currentKeys[i], true);
         }
@@ -878,7 +905,6 @@ extern "C" void app_main(void)
     };
     gpio_config(&capsLed);
 
-
     start_buzzer_task();
 
     const tinyusb_config_t tusb_cfg = {
@@ -902,51 +928,57 @@ extern "C" void app_main(void)
 
     // Initialize NVS.
     ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK( ret );
+    ESP_ERROR_CHECK(ret);
 
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
 
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     ret = esp_bt_controller_init(&bt_cfg);
-    if (ret) {
+    if (ret)
+    {
         ESP_LOGE(HID_DEMO_TAG, "%s initialize controller failed", __func__);
         return;
     }
 
     ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
-    if (ret) {
+    if (ret)
+    {
         ESP_LOGE(HID_DEMO_TAG, "%s enable controller failed", __func__);
         return;
     }
 
     ret = esp_bluedroid_init();
-    if (ret) {
+    if (ret)
+    {
         ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed", __func__);
         return;
     }
 
     ret = esp_bluedroid_enable();
-    if (ret) {
+    if (ret)
+    {
         ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed", __func__);
         return;
     }
 
-    if((ret = esp_hidd_profile_init()) != ESP_OK) {
+    if ((ret = esp_hidd_profile_init()) != ESP_OK)
+    {
         ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed", __func__);
     }
 
-    ///register the callback function to the gap module
+    /// register the callback function to the gap module
     esp_ble_gap_register_callback(gap_event_handler);
     esp_hidd_register_callbacks(hidd_event_callback);
 
     /* set the security iocap & auth_req & key size & init key response key parameters to the stack*/
-    esp_ble_auth_req_t auth_req = ESP_LE_AUTH_BOND;     //bonding with peer device after authentication
-    esp_ble_io_cap_t iocap = ESP_IO_CAP_NONE;           //set the IO capability to No output No input
-    uint8_t key_size = 16;      //the key size should be 7~16 bytes
+    esp_ble_auth_req_t auth_req = ESP_LE_AUTH_BOND; // bonding with peer device after authentication
+    esp_ble_io_cap_t iocap = ESP_IO_CAP_NONE;       // set the IO capability to No output No input
+    uint8_t key_size = 16;                          // the key size should be 7~16 bytes
     uint8_t init_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
     uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
     esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req, sizeof(uint8_t));
@@ -958,7 +990,7 @@ extern "C" void app_main(void)
     and the init key means which key you can distribute to the slave. */
     esp_ble_gap_set_security_param(ESP_BLE_SM_SET_INIT_KEY, &init_key, sizeof(uint8_t));
     esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key, sizeof(uint8_t));
-    
+
     // MAIN LOOP
 
     while (true)
@@ -973,7 +1005,7 @@ extern "C" void app_main(void)
                     if (i == col)
                     {
                         gpio_set_direction(cols[i], GPIO_MODE_OUTPUT_OD);
-                        gpio_set_level(cols[i],0);
+                        gpio_set_level(cols[i], 0);
                     }
                     else
                     {
